a6d14b73554d354a1e13900fad28c8a984a095f2
tirannosario <glt.rosario@gmail.com>
2020-08-20 15:23:12 +0200

    add skills & projects

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    //handle the navbar
     $(".nav-link").click(function (e) {
         e.preventDefault();
         var type = $(this).attr("id");
@@ -27,5 +28,15 @@ $( document ).ready(function() {
         }
         return false;
     });
+
+
+    $.getJSON('src/projects.json', function(data) {
+        var projectsbox = $(".projects-box");
+        var content = "";
+        data.forEach(project => content += "<div class=\"project-preview\"><div class=\"preview-img\"><img src="+project["preview-img"]+" alt=\"\"></div><div class=\"preview-content\"><p>"+project["preview"]+"</p><button title="+ project["name"] +">Open</button></div></div>");
+        projectsbox.html(content); // set the content
+    });
+
+
 });
 
